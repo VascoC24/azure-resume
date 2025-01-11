439284adcea49cebd0e52d6ac2ccf29750f29f06
unknown <vascosousa2407@gmail.com>
2025-01-11 01:06:12 +0000

    Changes in CORS and URLs

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecontador.azurewebsites.net/api/GetResumeCounter?code=p9JvmWaQmz3qSIs7B5nuzO3IFz4pUKJLxIAbPQDKDAlUAzFug6Rvzw%3D%3D'
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
